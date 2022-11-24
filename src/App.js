9d4c78ff564778feba9340b1b082bfa115460e35
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-11-24 13:59:08 +0300

    created the basic structure of the elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Todo} from "./components/Todo/Todo";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{id: 1, title: "JS", subtitle: "Web lang"},
+		{id: 2, title: "NodeJS", subtitle: "Web lang 1"},
+		{id: 3, title: "Webpack", subtitle: "Web lang 2"},
+    {id: 4, title: "Bootstrap", subtitle: "Web lang 3"}
+	]);
+
+	return (
+		<div className="App">
+			{todos.map((todo) => (
+				<Todo todo={todo} key={todo.id}/>
+			))}
+		</div>
+	);
 }
 
 export default App;
