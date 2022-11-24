44696d483ea1c111732a3d5f74e43dd2071db137
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-11-25 01:11:33 +0300

    added modal window for form-todo

---
+++ src/UI/modalWindow/MyModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './MyModal.module.css'
+
+export const MyModal = ({ children, visible, setVisible }) => {
+
+  const rootClasses = [style.myModal]
+
+  if (visible) {
+    rootClasses.push(style.active)
+  }
+
+  return (
+    <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+      <div className={style.myModalContent} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  )
+}
