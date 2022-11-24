3527208bceec50092d25453617de27147f23bce0
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-11-24 19:58:24 +0300

    added form for creating elements

---
+++ src/components/FormTodo/FormTodo.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { MyButton } from "../../UI/button/MyButton";
+import { MyInput } from "../../UI/input/MyInput";
+import style from "./FormTodo.module.css";
+
+export const FormTodo = ({ create }) => {
+  const [todo, setTodo] = useState({ title: "", subtitle: "" });
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      ...todo,
+      id: Date.now(),
+    };
+    create(newTodo);
+    setTodo({ title: "", subtitle: "" });
+  };
+
+  return (
+    <form className={style.addTodo}>
+      <MyInput value={todo.title} onChange={(e) => setTodo({ ...todo, title: e.target.value })} placeholder="Название задачи ...." />
+      <MyInput value={todo.subtitle} onChange={(e) => setTodo({ ...todo, subtitle: e.target.value })} placeholder="Описание задачи ...." />
+      <MyButton onClick={addNewTodo} text="Добавить задачу" />
+    </form>
+  );
+};
